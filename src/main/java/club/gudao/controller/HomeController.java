10202dd8c33c5bb3f8fe6c3f637e620705ed2451
Gudao <trailgudao@gmail.com>
2020-03-11 02:15:05 +0800

    初始界面

--- src/main/java/club/gudao/controller/HomeController.java
+++ src/main/java/club/gudao/controller/HomeController.java
@@ -1,17 +1,17 @@
 package club.gudao.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Author : GuDao
  * 2020/3/8
- * 佛曰：
- * 写字楼里写字间，写字间里程序员；
- * 程序人员写程序，又拿程序换酒钱；
- * 酒醒只在网上坐，酒醉还来网下眠；
- * 酒醉酒醒日复日，网上网下年复年；
- * 但愿老死电脑间，不愿鞠躬老板前；
- * 奔地宝马贵者趣，公交自行程序员；
- * 别人笑我忒疯癫，我笑自己命太贱；
  */
 
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String homePage(){
+        return "index";
+    }
 }

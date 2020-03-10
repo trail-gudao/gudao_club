47293990c57e028108d60f649883e64eca6a5b89
Gudao <trailgudao@gmail.com>
2020-03-11 02:35:56 +0800

    登陆界面完成

--- src/main/java/club/gudao/controller/HomeController.java
+++ src/main/java/club/gudao/controller/HomeController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String homePage(){
-        return "index";
+        return "login";
     }
 }
---
+++ src/main/java/club/gudao/controller/LoginController.java
@@ -0,0 +1,14 @@
+package club.gudao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Author : GuDao
+ * 2020/3/11
+ */
+
+@Controller
+@RequestMapping("login")
+public class LoginController {
+}
